ea112047dbfd3570c342433825ee6cfb49c80bac
Long Chen <154488668+lchen1307@users.noreply.github.com>
2025-12-31 16:47:56 +0800

    Add Footer component with copyright notice

---
+++ themes/thirdshire/components/Footer.js
@@ -0,0 +1,8 @@
+// themes/thirdshire/components/Footer.js
+export default function Footer() {
+  return (
+    <footer className="text-center py-8 text-sm text-gray-500">
+      © {new Date().getFullYear()} Carl Chanie. All rights reserved.
+    </footer>
+  );
+}
