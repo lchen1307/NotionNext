bbbc5e4d49468699954a4c5211bbc84e13891dd8
Long Chen <154488668+lchen1307@users.noreply.github.com>
2026-02-08 16:32:21 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -6,7 +6,7 @@ const BLOG = {
   NOTION_PAGE_ID:
     process.env.NOTION_PAGE_ID ||
     // '2c79d11fc50f815a89d3e7266a1c3d97,en:3019d11fc50f80a79f8add78da24ee84',
-    '2c79d11fc50f815a89d3e7266a1c3d97',
+    '2c79d11fc50f8150a669000c93ca7b20',
   THEME: process.env.NEXT_PUBLIC_THEME || 'hexo', // 当前主题，在themes文件夹下可找到所有支持的主题；主题名称就是文件夹名，例如 example,fukasawa,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
   LANG: process.env.NEXT_PUBLIC_LANG || 'zh-CN',  // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
   SINCE: process.env.NEXT_PUBLIC_SINCE || 2025, // e.g if leave this empty, current year will be used.
